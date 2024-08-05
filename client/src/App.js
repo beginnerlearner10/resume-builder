b17b70cb80ebe8dfb24b6b47791aac2a858f24a1
Rajrup Samanta <samantarajrup@gmail.com>
2024-08-05 22:05:45 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-center'>Let's build Resume</h1>
+    <Form/>
+    </div>
+  );
+}
+
+export default App;
